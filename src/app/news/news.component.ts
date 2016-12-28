3ee586385092a4da8089612ba94c3a1bf7e6bfcd
SludgeSlug <philip.wheldon@googlemail.com>
2016-12-28 22:37:00 +0000

    Adding paging to blog posts

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,21 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { BlogService } from '../shared';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { BlogService, NavigationService } from '../shared';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, OnDestroy {
 
   blogPosts: any;
+  olderPage: number;
+  newPage: number;
+  newsUrl: string;
+  private subscription: Subscription;
 
-  constructor(private blogService: BlogService) { }
+  constructor(
+    private blogService: BlogService,
+    private activatedRoute: ActivatedRoute,
+    private navigationService: NavigationService) { }
 
   ngOnInit() {
-    this.blogService.getMostRecentPosts()
-      .subscribe((blogPosts) => {
-        this.blogPosts = blogPosts;  
+    this.newsUrl = this.navigationService.news();
+    this.subscription = this.activatedRoute.queryParams.subscribe(
+      (param: any) => {
+        let pageNumber: number = Number.parseInt(param['page']);
+        this.blogService.getBlogPosts(pageNumber)
+          .subscribe((blogPosts) => {
+            this.blogPosts = blogPosts.entry;
+            let totalResults = blogPosts.openSearch$totalResults.$t;
+            let pageSize = blogPosts.openSearch$itemsPerPage.$t;
+            this.newPage = pageNumber - 1;
+            if (!pageNumber) {
+              pageNumber = 0;
+            }
+            if ((pageSize * (pageNumber + 1)) < totalResults) {
+              this.olderPage = pageNumber + 1;
+            } else {
+              this.olderPage = undefined;
+            }
+          });
       });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shared/blog.service.ts
+++ src/app/shared/blog.service.ts
@@ -7,18 +7,29 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BlogService {
-    private url = 'https://noizpromo.blogspot.co.uk/feeds/posts/default?alt=json-in-script&callback=JSONP_CALLBACK';
+    private url = 'https://noizpromo.blogspot.com//feeds/posts/default?alt=json-in-script&callback=JSONP_CALLBACK';
+    private pageSize: number = 5;
 
     constructor (private jsonp: Jsonp) {}
 
-    getMostRecentPosts(): Observable<any[]> {
+    getMostRecentPosts(): Observable<any> {
         let mostRecentPostsUrl = this.url + '&max-results=3';
         return this.jsonp.get(mostRecentPostsUrl)
                         .map(this.extractData);
     }
 
+    getBlogPosts(pageNumber): Observable<any> {
+        if (!pageNumber) {
+            pageNumber = 0;
+        }
+        let startIndex = (pageNumber * this.pageSize) + 1; 
+        let postsUrl = this.url + '&max-results=' + this.pageSize + '&start-index=' + startIndex;
+        return this.jsonp.get(postsUrl)
+                        .map(this.extractData);
+    }
+
     private extractData(res: Response) {
         let body = res.json();
-        return body.feed.entry;
+        return body.feed;
     }
 }
